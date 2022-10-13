7bbbfac215704fe89ca6ea461c958b435d1bcef3
ayushiaswasthi <113112565+ayushiaswasthi@users.noreply.github.com>
2022-10-13 15:16:28 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,14 @@
+from http.client import HTTPResponse
+
+
+from django.http import HttpResponse, JsonResponse
+
+
+def home_page(request):
+    print("home page requested")
+    friends=[
+        'ankit',
+        'ravi',
+        'uttam'
+    ]
+    return JsonResponse(friends,safe=False)
